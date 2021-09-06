f50de5b5d168aab4c97c8c5163e2f63f4958e064
Zevs <zevs@tutamail.com>
2021-09-06 01:08:25 -0700

    Add theme and theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+
 import { GlobalStyle } from '@styles/global';
+import { theme } from '@styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    background: '#000000',
+    text: {
+      primary: '#FFFFFF',
+      secondary: '#8F9BA8',
+    },
+  },
+};
