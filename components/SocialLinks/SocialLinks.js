51d9bdedf54337fd93a80ade81f385b9cc167ce9
Zevs <zevs@tutamail.com>
2021-09-06 09:29:27 -0700

    Add links to the social icons

--- components/SocialLinks/SocialLinks.js
+++ components/SocialLinks/SocialLinks.js
@@ -6,13 +6,13 @@ import * as S from './SocialLinks.styles';
 const SocialLinks = React.forwardRef((props, ref) => {
   return (
     <S.SocialContainer ref={ref}>
-      <S.SocialIcon href="#">
+      <S.SocialIcon href="https://instagram.com/zevsno">
         <IoLogoInstagram />
       </S.SocialIcon>
-      <S.SocialIcon href="#">
+      <S.SocialIcon href="https://twitter.com/zevsno">
         <IoLogoTwitter />
       </S.SocialIcon>
-      <S.SocialIcon href="#">
+      <S.SocialIcon href="https://github.com/zevsno">
         <IoLogoGithub />
       </S.SocialIcon>
     </S.SocialContainer>
