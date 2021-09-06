281c7c8173bb456247864bc021b8b76b99ef7d83
Zevs <zevs@tutamail.com>
2021-09-06 06:26:55 -0700

    Add meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import Hero from '@components/Hero';
 
 import * as S from '@styles/pages/Home.styles';
@@ -5,6 +7,20 @@ import * as S from '@styles/pages/Home.styles';
 const Home = () => {
   return (
     <S.Wrapper>
+      <Head>
+        <meta
+          name="description"
+          content="Front-end developer and content creator."
+        />
+        <meta
+          name="keywords"
+          content="zevs, developer, designer, content creator, font-end developer, privacy"
+        />
+        <meta name="theme-color" content="#000000" />
+        <title>Zevs — Front-end developer</title>
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+      </Head>
+
       <Hero />
     </S.Wrapper>
   );
